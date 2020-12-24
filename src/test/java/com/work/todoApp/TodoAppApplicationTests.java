f16fb6543391dee8e4adc5f2b2b0fd2171781011
abdullahyalcin <abdullah@leveltech.io>
2020-12-24 15:31:54 +0300

    gittignore configuration

---
+++ src/test/java/com/work/todoApp/TodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.work.todoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
